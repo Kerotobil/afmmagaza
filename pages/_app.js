58ad7b746be441c658933f44306fd21bc45264de
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2021-11-27 12:26:32 +0300

    edit app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,8 @@
 import React, { useEffect } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { Hydrate } from "react-query/hydration";
 import "tailwindcss/tailwind.css";
 
-import { ReactQueryDevtools } from "react-query/devtools";
-import axios from "axios";
-
 function MyApp({ Component, pageProps }) {
-  const queryClientRef = React.useRef();
-  if (!queryClientRef.current) {
-    queryClientRef.current = new QueryClient();
-  }
-
-  useEffect(() => {
-    if (process.browser) {
-      boot();
-    }
-  }, []);
-
-  return (
-    <QueryClientProvider client={queryClientRef.current}>
-      <Hydrate state={pageProps.dehydratedState}>
-        {process.browser && !ready && <div>'Yükleniyor...'</div>}
-        <Component {...pageProps} />
-      </Hydrate>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
